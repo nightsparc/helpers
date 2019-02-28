bfb78acdfd72e24a312b6dace641e2a2cf41f617
nightsparc <17517082+nightsparc@users.noreply.github.com>
2019-02-28 13:01:10 +0100

    Added script to remove a git submodule

---
+++ git/remove-submodule.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# From: https://gist.github.com/moyaldror/63b4c2b601592aa3ae8a317adec00a1c 
+
+if [ $# -ne 1 ]; then
+        echo "Usage: $0 <submodule full name>"
+        exit 1
+fi
+
+MODULE_NAME=$1
+MODULE_NAME_FOR_SED=$(echo $MODULE_NAME | sed -e 's/\//\\\//g')
+
+cat .gitmodules | sed -ne "/^\[submodule \"$MODULE_NAME_FOR_SED\"/,/^\[submodule/!p" > .gitmodules.tmp
+mv .gitmodules.tmp .gitmodules
+git add .gitmodules
+cat .git/config | sed -ne "/^\[submodule \"$MODULE_NAME_FOR_SED\"/,/^\[submodule/!p" > .git/config.tmp
+mv .git/config.tmp .git/config
+git rm --cached $MODULE_NAME
+rm -rf .git/modules/$MODULE_NAME
+rm -rf $MODULE_NAME
