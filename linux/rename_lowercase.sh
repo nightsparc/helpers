362e4dd0ee6bbd7504f20bbfd56058558d5b5227
nightsparc <17517082+nightsparc@users.noreply.github.com>
2019-06-21 21:53:58 +0200

    Added script to make all files and folders in given folder lower-case

---
+++ linux/rename_lowercase.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ROOT_DIR=$1
+
+for SRC in `find ${ROOT_DIR} -depth`
+do
+    DST=`dirname "${SRC}"`/`basename "${SRC}" | tr '[A-Z]' '[a-z]'`
+    if [ "${SRC}" != "${DST}" ]
+    then
+        [ ! -e "${DST}" ] && mv -T "${SRC}" "${DST}" || echo "${SRC} was not renamed"
+    fi
+done
